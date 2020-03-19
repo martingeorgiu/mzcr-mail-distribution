b5ac47c11aab5b5d84c559931e43db570d7d79ab
Martin Georgiu <login@georgiu.cz>
2020-03-20 00:04:14 +0100

    add credit

--- resources/views/finished.blade.php
+++ resources/views/finished.blade.php
@@ -2,4 +2,9 @@
 
 @section('content')
 <h2>Hotovo</h2>
+<h5>
+	vytvořil <a href="mailto:martin@georgiu.cz">martin@georgiu.cz</a>
+	z <a href="https://bindworks.eu/">bindworks.eu</a>
+	v rámci dobrovolné bezplatné pomoci
+</h5>
 @endsection
